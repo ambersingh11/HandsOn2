b16dceb34807edd12ddbdef1ff34d53a6dbd3e01
Amber Singh <ambersingh121@rediffmail.com>
2016-02-10 15:25:55 +0530

    iteration3

---
+++ TestingHandsOn/src/com/sapient/tests/SampleTestSuite.java
@@ -0,0 +1,17 @@
+package com.sapient.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+@RunWith(Suite.class)
+@SuiteClasses(value={BankTest.class,PerfectNumberTest.class})
+public class SampleTestSuite {
+
+	
+
+}
